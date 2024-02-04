e695c3262dbfbbf90dbabf353b940a6c52992b9f
Tea BinXiong <65224341+teabinxiong@users.noreply.github.com>
2024-02-05 00:57:37 +0800

    Add repositories & unit of work to the domain

---
+++ MyBooking.Domain/Users/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBooking.Domain.Users
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid, CancellationToken cancellationToken = default);
+
+        void Add(User user);
+    }
+}
