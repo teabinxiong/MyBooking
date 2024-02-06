8d4d27dac1f51ef9ff451d81a6e5bbd9c9e8696c
Tea BinXiong <65224341+teabinxiong@users.noreply.github.com>
2024-02-07 05:11:21 +0800

    Add Reserve Booking command and the handler

---
+++ MyBooking.Application/Bookings/ReserveBooking/ReserveBookingCommand.cs
@@ -0,0 +1,17 @@
+﻿using MyBooking.Application.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyBooking.Application.Bookings.ReserveBooking
+{
+    public record ReserveBookingCommand(
+        Guid ApartmentId,
+        Guid UserId,
+        DateOnly StartDate,
+        DateOnly EndDate
+        ) : ICommand<Guid>;
+}
---
+++ MyBooking.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -0,0 +1,78 @@
+﻿using MyBooking.Application.Abstractions.Clock;
+using MyBooking.Application.Abstractions.Messaging;
+using MyBooking.Domain.Abstractions;
+using MyBooking.Domain.Apartments;
+using MyBooking.Domain.Bookings;
+using MyBooking.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBooking.Application.Bookings.ReserveBooking
+{
+    internal sealed class ReserveBookingCommandHandler : ICommandHandler<ReserveBookingCommand, Guid>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IApartmentRepository _apartmentRepository;
+        private readonly IBookingRepository _bookingRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly PricingService _pricingService;
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public ReserveBookingCommandHandler(
+            IUserRepository userRepository, 
+            IApartmentRepository apartmentRepository,
+            IBookingRepository bookingRepository, 
+            IUnitOfWork unitOfWork, 
+            PricingService pricingService,
+            IDateTimeProvider dateTimeProvider
+            )
+        {
+            _userRepository = userRepository;
+            _apartmentRepository = apartmentRepository;
+            _bookingRepository = bookingRepository;
+            _unitOfWork = unitOfWork;
+            _pricingService = pricingService;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public async Task<Result<Guid>> Handle(ReserveBookingCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.UserId, cancellationToken);
+
+            if(user is null) {
+                return Result.Failure<Guid>(UserErrors.NotFound);
+            }
+
+            var apartment = await _apartmentRepository.GetByIdAsync(request.ApartmentId, cancellationToken);
+
+            if(apartment is null)
+            {
+                return Result.Failure<Guid>(ApartmentErrors.NotFound);
+            }
+
+            var duration = DateRange.Create(request.StartDate, request.EndDate);
+
+            if(await _bookingRepository.IsOverlappingAsync(apartment, duration, cancellationToken))
+            {
+                return Result.Failure<Guid>(BookingErrors.Overlap);
+            }
+
+            var booking = Booking.Reserve(
+                apartment,
+                user.Id,
+                duration,
+                utcNow: _dateTimeProvider.UtcNow,
+                _pricingService
+                );
+
+            _bookingRepository.Add(booking);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return booking.Id;
+        }
+    }
+}
